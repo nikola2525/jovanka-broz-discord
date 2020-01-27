1fd6cf43c06a3d2780a7944ffc5f19f7324d3f21
Mario Durasin <mario.durasin@undabot.com>
2020-01-27 16:12:42 +0000

    Do not retry writes on DB

--- lib/database_manager.py
+++ lib/database_manager.py
@@ -7,7 +7,7 @@ from uritools import urisplit
 class DBM:
     connection_uri = environ['MONGODB_URI']
     db_name = urisplit(connection_uri).path[1:]
-    client = motor.motor_asyncio.AsyncIOMotorClient(connection_uri, retry_writes=False)
+    client = motor.motor_asyncio.AsyncIOMotorClient(connection_uri, retryWrites=False)
 
     db = client[db_name]
 
